fbd1db52a475756de869f31bcd5d507d969d6401
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-11-18 18:45:31 -0500

    created api calls for squares to update and get squares

---
+++ api/squareData.js
@@ -0,0 +1,41 @@
+const endpoint = 'http://localhost:7188';
+
+const getSingleSquare = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/api/squares/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((resp) => {
+      if (resp.status === 404) {
+        resolve({});
+      } else {
+        resolve(resp.json());
+      }
+    })
+    .catch(reject);
+});
+
+const updateSquare = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/api/squares/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((resp) => {
+      if (resp.status === 204) {
+        resolve({});
+      } else {
+        resolve(resp.json());
+      }
+    })
+    .catch(reject);
+});
+
+export {
+  getSingleSquare,
+  updateSquare,
+};
