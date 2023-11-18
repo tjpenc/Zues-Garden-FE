b96db966fdd760fee36145b12376cd7fcc4422ad
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-11-18 18:42:14 -0500

    created bed api calls to create, update, delete, and get beds

---
+++ api/bedData.js
@@ -0,0 +1,96 @@
+const endpoint = 'http://localhost:7188';
+
+const getBeds = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/api/beds/${uid}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((resp) => {
+      if (resp.status === 404) {
+        resolve([]);
+      } else {
+        resolve(resp.json());
+      }
+    })
+    .catch(reject);
+});
+
+const getSingleBed = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/api/beds/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((resp) => {
+      if (resp.status === 404) {
+        resolve({});
+      } else {
+        resolve(resp.json());
+      }
+    })
+    .catch(reject);
+});
+
+const createBed = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/api/beds`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((resp) => {
+      if (resp.status === 204) {
+        resolve({});
+      } else {
+        resolve(resp.json());
+      }
+    })
+    .catch(reject);
+});
+
+const updateBed = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/api/beds/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((resp) => {
+      if (resp.status === 204) {
+        resolve({});
+      } else {
+        resolve(resp.json());
+      }
+    })
+    .catch(reject);
+});
+
+const deleteBed = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/api/beds/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((resp) => {
+      if (resp.status === 404) {
+        resolve('Bed not found');
+      } else {
+        resolve(resp.json());
+      }
+    })
+    .catch(reject);
+});
+
+export {
+  getBeds,
+  getSingleBed,
+  createBed,
+  updateBed,
+  deleteBed,
+};
