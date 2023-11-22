f88a7ced78459ad52572970292200be4e1b8ad08
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-11-22 08:37:30 -0500

    screens now show loading spinner when waiting for promises to return

--- pages/tasks/tasks.js
+++ pages/tasks/tasks.js
@@ -4,38 +4,50 @@ import { Button } from 'react-bootstrap';
 import SmallTaskCard from '../../components/cards/TaskCardSmall';
 import { useAuth } from '../../utils/context/authContext';
 import { getTasks } from '../../api/taskData';
+import Loading from '../../components/Loading';
 
 export default function ViewTasks() {
   const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const { user } = useAuth();
 
-  const getAllTasks = () => getTasks(user.uid).then(setTasks);
+  const getAllTasks = () => getTasks(user.uid).then((tasksArray) => {
+    setTasks(tasksArray);
+    setIsLoading(false);
+  });
 
   useEffect(() => {
     setTimeout(() => {
       getAllTasks();
-    }, 500);
+    }, 300);
   }, []);
 
   return (
     <>
-      <div className="mt-3">
-        <Link passHref href="/tasks/createTask">
-          <Button>Create Task</Button>
-        </Link>
-      </div>
-      <h1 className="center mb-5">My Tasks</h1>
-      <div className="space-around mt-3 wrap">
-        {tasks?.length === 0
-          ? (
-            <>
-              <div>
-                <h2>You have no current tasks! Use the top left button to create a new task</h2>
-              </div>
-            </>
-          )
-          : tasks?.map((task) => <SmallTaskCard key={task.id} taskObj={task} onUpdate={getAllTasks} />)}
-      </div>
+      {isLoading
+        ? <Loading />
+        : (
+          <>
+            <div className="mt-3">
+              <Link passHref href="/tasks/createTask">
+                <Button>Create Task</Button>
+              </Link>
+            </div>
+            <h1 className="center mb-5">My Tasks</h1>
+            <div className="space-around mt-3 wrap">
+              {tasks?.length === 0
+                ? (
+                  <>
+                    <div>
+                      <h2>You have no current tasks! Use the top left button to create a new task</h2>
+                    </div>
+                  </>
+                )
+                : tasks?.map((task) => <SmallTaskCard key={task.id} taskObj={task} onUpdate={getAllTasks} />)}
+            </div>
+          </>
+        )}
+
     </>
   );
 }
