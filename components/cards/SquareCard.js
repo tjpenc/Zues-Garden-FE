1accd0505781d837507d615e85b06cec2e7b6c24
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-12-19 17:43:19 -0500

    plant name appears on hover over a square in a bed

--- components/cards/SquareCard.js
+++ components/cards/SquareCard.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 export default function SquareCard({ squareObj, bedWidth, bedLength }) {
+  const [isHovering, setIsHovering] = useState(false);
   const tempFix = () => {
     const mult = bedWidth * bedLength;
     return mult;
@@ -10,6 +12,18 @@ export default function SquareCard({ squareObj, bedWidth, bedLength }) {
     tempFix();
   }
 
+  const changeHover = () => {
+    setIsHovering((prevState) => !prevState);
+  };
+
+  const handleMouseHover = (e) => {
+    if (e.type === 'mouseover') {
+      setTimeout(changeHover, 2000);
+    } else if (e.type === 'mouseout') {
+      changeHover(false);
+    }
+  };
+
   return (
     <>
       <div
@@ -17,8 +31,13 @@ export default function SquareCard({ squareObj, bedWidth, bedLength }) {
         style={{
           flex: `0 1 ${(1 / bedLength) * 100}%`,
         }}
+        // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
+        onMouseOver={handleMouseHover}
+        // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
+        onMouseOut={handleMouseHover}
       >
-        {squareObj.plant ? `${squareObj.plant.symbol}` : ''}
+        {squareObj.plant && !isHovering ? `${squareObj.plant.symbol}` : ''}
+        {squareObj?.plant && isHovering ? squareObj?.plant.name : ''}
       </div>
     </>
   );
